7eee7fd7de657005c89e74bbfb49007f34aba49a
tulio <tulio@uppoints.com>
2017-11-07 12:31:51 -0200

    add search

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListAllBooks from './components/ListAllBooks'
+import SearchBook from './components/SearchBook'
+
 
 class BooksApp extends React.Component {
   state = {
     books: [],
+    booksShearch: [],
+    search: '',
   }
 
   componentDidMount() {
@@ -15,39 +19,21 @@ class BooksApp extends React.Component {
 
   updateBook = (book, shelf) => BooksAPI.update(book, shelf).then(e => this.loadBooks())
 
-  loadBooks = () => {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
+  shearchBook = (query) => {
+    this.setState({ search: query })
+    BooksAPI.search(query, 20).then((booksShearch) => this.setState({ booksShearch })).catch(e => this.setState({ booksShearch: [] }))
   }
+
+  loadBooks = () => BooksAPI.getAll().then((books) => this.setState({ books }))
 
   render() {
     return (
       <div className="app">
         <Route path='/search' render={({ history }) => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => history.push('/')}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBook search={this.state.shearch} booksShearch={this.state.booksShearch} onUpdateBook={this.updateBook} onShearchBook={this.shearchBook} onClickReturn={() => history.push('/')} />
         )} />
         <Route exact path='/' render={({ history }) => (
-          <ListAllBooks books={this.state.books} onUpdateBook={this.updateBook} onClickSearch={()=>history.push('/search')}/>
+          <ListAllBooks books={this.state.books} onUpdateBook={this.updateBook} onClickSearch={() => history.push('/search')} />
         )} />
       </div>
     )
