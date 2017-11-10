d327109439af196c19d7a3df5281dcd0f805cb17
tulio <tulio@uppoints.com>
2017-11-10 15:20:45 -0200

    onchange

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -19,7 +19,7 @@ function BookView(props) {
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.smallThumbnail})` }}></div>
         <div className="book-shelf-changer">
-          <select defaultValue={shelfSelect} onClick={(e) => props.onUpdateBook(props.book, e.target.value)}>
+          <select defaultValue={shelfSelect} onChange={(e) => props.onUpdateBook(props.book, e.target.value)}>
             <option value="none" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
