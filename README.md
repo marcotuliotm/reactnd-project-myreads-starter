c518721f0c02ad91cce6aaf12b5047a2d301b6aa
Tulio <Tulio@MacBook-Air-de-EDUARDO.local>
2017-11-11 22:06:25 -0200

    fix criticals

--- README.md
+++ README.md
@@ -88,6 +88,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookView from './BookView'
 import PropTypes from 'prop-types'
 import { DebounceInput } from 'react-debounce-input'
+import Loader from 'halogen/PulseLoader'
 
 
-class SearchBook extends Component {
+function SearchBook(props) {
 
-  static propTypes = {
-    booksShearch: PropTypes.array.isRequired,
-    onUpdateBook: PropTypes.func.isRequired,
-    onShearchBook: PropTypes.func.isRequired,
-    onClickReturn: PropTypes.func.isRequired,
-    search: PropTypes.string.isRequired,
-  }
+  const { booksShearch, onUpdateBook, search, onShearchBook, onClickReturn, loading } = props
 
-  render() {
-    const { booksShearch, onUpdateBook, search, onShearchBook, onClickReturn } = this.props
-
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <a className="close-search" onClick={() => onClickReturn()}>Close</a>
-          <div className="search-books-input-wrapper">
-            <DebounceInput minLength={2} debounceTimeout={500} type="text" placeholder="Search by title or author" value={search} onChange={(event) => onShearchBook(event.target.value)} />
-          </div>
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a className="close-search" onClick={() => onClickReturn()}>Close</a>
+        <div className="search-books-input-wrapper">
+          <DebounceInput minLength={2} debounceTimeout={500} type="text" placeholder="Search by title or author" value={search} onChange={(event) => onShearchBook(event.target.value)} />
         </div>
-        <div className="search-books-results">
+      </div>
+      <div className="search-books-results">
+        {loading ? (<Loader color="#26A65B" size="16px" margin="4px" />) : (
           <ol className="books-grid">
             {booksShearch.map((book) => (
               <li key={book.id}>
@@ -33,10 +26,19 @@ class SearchBook extends Component {
               </li>
             ))}
           </ol>
-        </div>
+        )}
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+SearchBook.propTypes = {
+  booksShearch: PropTypes.array.isRequired,
+  onUpdateBook: PropTypes.func.isRequired,
+  onShearchBook: PropTypes.func.isRequired,
+  onClickReturn: PropTypes.func.isRequired,
+  search: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
 export default SearchBook
